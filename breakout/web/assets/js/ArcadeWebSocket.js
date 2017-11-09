25bbcfe92729bba631aec5558b83e4d9d5280286
Unknown <ben.de.boevere@student.howest.be>
2017-11-09 11:19:58 +0100

    Refactoring js
    Add Level, Brick and ArcadeWebSocket objects. Cleaned up all js files.

---
+++ breakout/web/assets/js/ArcadeWebSocket.js
@@ -0,0 +1,76 @@
+var ArcadeWebSocket = function () {
+
+    this.gameId = getParameterByName("gameId");
+    this.wsUri = "ws://" + document.location.host + "/breakout/gameplay?gameId=" + this.gameId;
+    this.websocket = new WebSocket(this.wsUri);
+
+    this.websocket.onerror = function (evt) {
+        onError(evt);
+    };
+    this.websocket.onopen = function (evt) {
+        onOpen(evt);
+    };
+
+    this.websocket.onmessage = function (evt) {
+        onMessage(evt);
+    };
+
+};
+
+ArcadeWebSocket.prototype.sendOverSocket = function (json) {
+    if (this.websocket.readyState === this.websocket.OPEN) {
+        this.websocket.send(json);
+    }
+}
+
+ArcadeWebSocket.prototype.close = function () {
+    this.websocket.close();
+}
+
+function onOpen(evt) {
+    console.log('Connection open');
+}
+
+function onError(evt) {
+    console.error(evt.message);
+}
+
+function onMessage(evt) {
+    var json = JSON.parse(evt.data);
+
+    if (json && !json.error) {
+
+        if (json.gameOver) {
+            console.log("Gameplay: game over");
+            gameOverMessage();
+            level.gameOver = true;
+        } else if (json.livesLeft) {
+            console.log("Gameplay: livesLeft: " + json.livesLeft);
+            loadLives(json.livesLeft);
+        } else if (json.levelComplete) {
+            console.log("Socket received message level complete");
+            level.levelComplete = true;
+
+            level.loadLevel();
+
+        } else {
+            level.updateLevelData(json);
+        }
+    } else {
+        handleLevelUpdateError(json);
+    }
+
+}
+
+function handleLevelUpdateError(json) {
+    if (json.error) {
+        document.location = "/breakout?error=" + json.error;
+    } else
+    {
+        document.location = "/breakout?error=Something went wrong";
+    }
+}
+
+
+
+
--- breakout/web/assets/js/script.js
+++ breakout/web/assets/js/script.js
@@ -1,15 +1,13 @@
-$(document).ready(function(){
-    $("#returnToMain").on("click",redirectToMainMenu);
-    $("#toMultiplayer").on("click",redirectToMultiplayer);
-});
 var redirectToMainMenu=function(e){
     e.preventDefault();
     location.replace('index.html');
 };
+
 var redirectToMultiplayer=function(e){
     e.preventDefault();
     location.replace('multiplayerMenu.html');
 };
+
 var loadLives=function(lives){
     var imagePath="assets/media/ankLife.png";
     var lifeImages="";
@@ -18,6 +16,12 @@ var loadLives=function(lives){
     }
     $("#healthbar").html(lifeImages);
 };
+
 var gameOverMessage=function(){
     $("#healthbar").html("<h1>Game over</h1>");
 }
+
+$(document).ready(function(){
+    $("#returnToMain").on("click",redirectToMainMenu);
+    $("#toMultiplayer").on("click",redirectToMultiplayer);
+});
